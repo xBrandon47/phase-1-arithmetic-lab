a79d39029a3c08e1ae973a44999e0de2bfa9ac5c
Brandon Moreno <brandonm99@me.com>
2022-09-19 17:29:55 -0600

    commit tree 0348d3cb2114cedd82bc88787c20cbd9621c503c

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor( Math.random() * 1000 ) + 1;
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max( 5, 10, 15, 20 );
